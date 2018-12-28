f40281370b483c5ea4d235b1d8a9b4f9fb006655
Natalie Dodd <nataliedodd89@gmail.com>
2018-12-28 15:21:38 -0500

    feat: set authedUser - create action and reducer for authedUser - update page title - update Login form with button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { getInitialUsers } from '../actions/shared';
-import { connect } from 'react-redux'
+import { setAuthedUser } from '../actions/authedUser';
+import { connect } from 'react-redux';
 
-class App extends Component {
+class Login extends Component {
+
+  state = {
+    selectedUser: {}
+  }
 
   componentDidMount() {
     this.props.dispatch(getInitialUsers())
+  }
+
+  handleSelectUser = e => {
+    const selectedUser = this.props.users[e.target.value]
+    this.setState(() => ({
+      selectedUser
+    }));
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.dispatch(setAuthedUser(this.state.selectedUser))
   }
 
   render() {
@@ -14,9 +31,32 @@ class App extends Component {
         <h1>
           Login
         </h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="user-select">Choose a user:</label>
+          <select id="user-select" onChange={this.handleSelectUser}>
+            <option value="">--Please choose an option--</option>
+            {this.props.users && Object.keys(this.props.users).map(user => (
+              <option
+                key={this.props.users[user].id}
+                value={user}
+              >
+                {this.props.users[user].name}
+              </option>
+            ))}
+          </select>
+          <button type='submit'>
+            Login
+          </button>
+        </form>
       </div>
     )
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ users }) {
+  return {
+    users
+  }
+}
+
+export default connect(mapStateToProps)(Login);
