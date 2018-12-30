b5319a83b4a5cdba4db0b91dc956aa809830befb
Natalie Dodd <nataliedodd89@gmail.com>
2018-12-30 16:04:52 -0500

    feat: add styles for Leaderboard and Nav

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import styles from './styles/Leaderboard.module.scss';
 
 class Leaderboard extends Component {
   render() {
     return (
-      <div>
-        <h1>Leaderboard</h1>
-        {Object.keys(this.props.users).map(id => {
+      <div className={styles.container}>
+        <h1 className={styles.title}>Leaderboard</h1>
+        {Object.keys(this.props.users).map((id, index) => {
           const user = this.props.users[id]
           return (
-            <div key={user.id}>
-              <p>{user.name}</p>
+            <div key={user.id} className={styles.card}>
+              <span className={styles.ranking}>{index + 1}</span>
+              <h2 className={styles.userName}>{user.name}</h2>
               <img alt="User Avatar" width="35px" src={user.avatarURL} />
               <p>Questions Asked: {user.questions.length}</p>
               <p>Questions Answered: {Object.keys(user.answers).length}</p>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { logoutUser } from '../actions/authedUser';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import styles from './styles/Nav.module.scss';
+import { withRouter } from 'react-router-dom';
 
 class Nav extends Component {
 
@@ -13,32 +15,44 @@ class Nav extends Component {
 
   render() {
     return (
-      <div>
-        <ul>
-          <li>
-            <NavLink to='/' exact activeClassName='active'>
-              Home
-            </NavLink>
+      <ul className={styles.nav}>
+        <li className={styles.navItem}>
+          <NavLink
+            to='/'
+            exact
+            activeClassName={styles.active}
+            className={styles.link}>
+            Home
+          </NavLink>
+        </li>
+        <li className={styles.navItem}>
+          <NavLink
+            to='/leaderboard'
+            exact
+            activeClassName={styles.active}
+            className={styles.link}>
+            LeaderBoard
+          </NavLink>
+        </li>
+        <li className={styles.navItem}>
+          <NavLink
+            to='/add'
+            exact
+            activeClassName={styles.active}
+            className={styles.link}>
+            New Poll
+          </NavLink>
+        </li>
+        {this.props.authedUser && (
+          <li className={styles.navItem}>
+            <button
+              onClick={this.logout}
+              className={styles.link}>
+              Logout
+            </button>
           </li>
-          <li>
-            <NavLink to='/leaderboard' activeClassName='active'>
-              LeaderBoard
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/add' activeClassName='active'>
-              New Poll
-            </NavLink>
-          </li>
-          {this.props.authedUser && (
-            <li>
-              <button onClick={this.logout}>
-                Logout
-              </button>
-            </li>
-          )}
-        </ul>
-      </div>
+        )}
+      </ul>
     )
   }
 }
@@ -49,4 +63,4 @@ function mapStateToProps({ authedUser }) {
   }
 }
 
-export default connect(mapStateToProps)(Nav);
+export default withRouter(connect(mapStateToProps)(Nav));
